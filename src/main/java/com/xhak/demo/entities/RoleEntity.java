4b0b9e4d8dd3d1acda41f5a53e1c4505c0346477
kevkolleshi95 <kejvi1995@gmail.com>
2024-05-25 15:05:46 +0200

    test

--- src/main/java/com/xhak/demo/entities/RoleEntity.java
+++ src/main/java/com/xhak/demo/entities/RoleEntity.java
@@ -1,4 +1,26 @@
 package com.xhak.demo.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table
+@Entity
 public class RoleEntity {
+    @Id
+    private Long id;
+
+
+    @Column(nullable = false)
+    private String user;
+
+    @Column(nullable = false)
+    private String admin;
 }
--- src/main/java/com/xhak/demo/entities/UserEntity.java
+++ src/main/java/com/xhak/demo/entities/UserEntity.java
@@ -1,4 +1,38 @@
 package com.xhak.demo.entities;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table
+@Entity
 public class UserEntity {
+    @Id
+    private Long id;
+
+
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String subscribtion;
+
+    @Column(nullable = false)
+    private String role;
 }
