1a5a75b0c66df8e3fbad6f7b2aa8fe4bc3bbc88b
Silvjo Tringoski <67462300+lethalbonsai@users.noreply.github.com>
2024-05-25 11:54:50 +0200

    first commit

---
+++ src/test/java/com/xhak/demo/SkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xhak.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
