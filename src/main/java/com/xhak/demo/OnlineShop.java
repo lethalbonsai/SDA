0044280ede1f23d941a93e2f9ed0063f50b46ea4
kevkolleshi95 <kejvi1995@gmail.com>
2024-06-09 10:36:42 +0200

    last commit

--- src/main/java/com/xhak/demo/OnlineShop.java
+++ src/main/java/com/xhak/demo/OnlineShop.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OnlineShop {
-
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShop.class, args);
 	}
